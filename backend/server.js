4143b52a1b483960bba979c34381cc800d169efa
dzelga <dorota.zelga@gmail.com>
2019-07-27 11:05:12 +0200

    listen port > line commented

--- backend/server.js
+++ backend/server.js
@@ -3,4 +3,5 @@ const express = require('express');
 const API_PORT = 3001;
 const app = express();
 
+// launch server into a port
 app.listen(API_PORT, () => console.log('Server is up and running on port number ' + API_PORT));
