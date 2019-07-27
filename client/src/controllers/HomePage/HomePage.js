5557967d475d09b4c9427b653d08d81f85342143
dzelga <dorota.zelga@gmail.com>
2019-07-27 13:22:19 +0200

    HomePage > add search input

--- client/src/controllers/HomePage/HomePage.js
+++ client/src/controllers/HomePage/HomePage.js
@@ -1,9 +1,27 @@
 import React, {Component} from 'react';
 
 class HomePage extends Component {
+    constructor() {
+        super();
+        this.state = {
+            searchedValue: ""
+        };
+    }
+
+    searchValue = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
 
     render() {
-        return <div></div>
+        return <div>
+            <input className="form-control"
+                   type="text"
+                   name="searchedValue"
+                   onChange={this.searchValue}/>
+        </div>
     }
 }
 
