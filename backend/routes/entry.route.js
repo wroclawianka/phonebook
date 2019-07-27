43e5e1391b8799d6857d7b46bffc6ac48e321fc7
dzelga <dorota.zelga@gmail.com>
2019-07-27 14:39:27 +0200

    get entries according to the query

--- backend/routes/entry.route.js
+++ backend/routes/entry.route.js
@@ -4,10 +4,18 @@ const Entry = require('../models/entry.model');
 
 // get entries
 router.get('/get/entry', (req, res) => {
-    Entry.find((err, data) => {
-        if (err) return res.json({success: false, error: err});
-        return res.json({success: true, data: data});
-    });
+    const value = req.query.value;
+    Entry.find(
+        {
+            "$or": [
+                {"firstName": {$regex: `.*${value}.*`}},
+                {"lastName": {$regex: `.*${value}.*`}},
+                {"phoneNumber": {$regex: `.*${value}.*`}}
+            ]
+        }, (err, data) => {
+            if (err) return res.json({success: false, error: err});
+            return res.json({success: true, data: data});
+        });
 });
 
 module.exports = router;
