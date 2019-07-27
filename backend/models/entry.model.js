1ee22b76d31a244e687aee354d771b1c762ab8e3
dzelga <dorota.zelga@gmail.com>
2019-07-27 12:17:15 +0200

    models > entry model created

---
+++ backend/models/entry.model.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const EntrySchema = new Schema(
+    {
+        id: Number,
+        firstName: String,
+        lastName: String,
+        phoneNumber: String
+    }
+);
+
+module.exports = mongoose.model("Entry", EntrySchema, "entry");
