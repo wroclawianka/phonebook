bdc1421634a5796bdd9d3363b0b07ebfa50bf90c
dzelga <dorota.zelga@gmail.com>
2019-07-27 13:00:27 +0200

    App.js as react component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function App() {
-    return (
-        <div/>
-    );
+class App extends Component {
+    render() {
+        return <div/>
+    }
 }
 
 export default App;
