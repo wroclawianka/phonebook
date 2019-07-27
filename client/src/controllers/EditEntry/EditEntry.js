5891f0e9043c2e48449ca5db47a9a787cd9b9641
dzelga <dorota.zelga@gmail.com>
2019-07-27 18:28:31 +0200

    EditEntry > componentDidMount > get/entry/:id and setState

--- client/src/controllers/EditEntry/EditEntry.js
+++ client/src/controllers/EditEntry/EditEntry.js
@@ -1,6 +1,19 @@
 import React, {Component} from 'react';
+import axios from 'axios';
+import api from "../../api.json"
 
 class EditEntry extends Component {
+    constructor() {
+        super();
+        this.state = {}
+    }
+
+    componentDidMount() {
+        const {match: {params}} = this.props;
+        axios.get(api.url + `get/entry/${params.id}`)
+            .then(({data: res}) => this.setState({...res.data}))
+            .catch((err) => console.error(err))
+    }
 
     render() {
         return <div></div>
