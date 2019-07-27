d07acd5d48162f5a88afe339e0fee5ce6eae1eb3
dzelga <dorota.zelga@gmail.com>
2019-07-27 16:20:19 +0200

    App.js > routing added

--- client/src/controllers/App.js
+++ client/src/controllers/App.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import HomePage from './HomePage/HomePage.js'
+import AddEntry from './AddEntry/AddEntry'
+import EditEntry from './EditEntry/EditEntry'
 
 class App extends Component {
     render() {
         return <div>
             <h1>Phonebook</h1>
-            <HomePage/>
+            <Router>
+                <Route exact path='/' component={HomePage}/>
+                <Route path='/add-entry' component={AddEntry}/>
+                <Route path='/edit-entry' component={EditEntry}/>
+            </Router>
         </div>
     }
 }
