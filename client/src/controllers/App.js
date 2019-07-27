0098c6d4fe9f3e2a548eb2989b7f07d3d32acbcd
dzelga <dorota.zelga@gmail.com>
2019-07-27 21:10:01 +0200

    main header as link do homepage

--- client/src/controllers/App.js
+++ client/src/controllers/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import './App.css';
 import HomePage from './HomePage/HomePage.js'
 import AddEntry from './AddEntry/AddEntry'
 import EditEntry from './EditEntry/EditEntry'
@@ -7,7 +8,7 @@ import EditEntry from './EditEntry/EditEntry'
 class App extends Component {
     render() {
         return <div>
-            <h1>Phonebook</h1>
+            <h1><a className="homepage-link" href="/">Phonebook</a></h1>
             <Router>
                 <Route exact path='/' component={HomePage}/>
                 <Route path='/add-entry' component={AddEntry}/>
