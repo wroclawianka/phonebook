aa6f12c96058148d18c67ef17f31fa90994de778
dzelga <dorota.zelga@gmail.com>
2019-07-27 16:31:43 +0200

    AddEntry > form created

--- client/src/controllers/AddEntry/AddEntry.js
+++ client/src/controllers/AddEntry/AddEntry.js
@@ -1,10 +1,38 @@
 import React, {Component} from 'react';
 
 class AddEntry extends Component {
+    constructor() {
+        super();
+        this.state = {
+            firstName: "",
+            lastName: "",
+            phoneNumber: ""
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+    }
 
     render() {
-        return <div></div>
-
+        return <div>
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" name="firstName" placeholder="First Name" onChange={this.handleChange}/>
+                <br/>
+                <input type="text" name="lastName" placeholder="Last Name" onChange={this.handleChange}/>
+                <br/>
+                <input type="text" name="phoneNumber" placeholder="Phone number" onChange={this.handleChange}/>
+                <br/>
+                <br/>
+                <input type="submit" value="Add entry"/>
+            </form>
+        </div>
     }
 }
 
