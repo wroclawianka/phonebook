01dd6767fff10f867dfd8f5d803452ccc92d7920
dzelga <dorota.zelga@gmail.com>
2019-07-28 08:08:17 +0200

    App/js > Box component replaced main div

--- client/src/controllers/App.js
+++ client/src/controllers/App.js
@@ -1,20 +1,21 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
+import Box from '@material-ui/core/Box';
 import HomePage from './HomePage/HomePage.js'
 import AddEntry from './AddEntry/AddEntry'
 import EditEntry from './EditEntry/EditEntry'
 
 class App extends Component {
     render() {
-        return <div>
+        return <Box component="div">
             <h1><a className="homepage-link" href="/">Phonebook</a></h1>
             <Router>
                 <Route exact path='/' component={HomePage}/>
                 <Route path='/add-entry' component={AddEntry}/>
                 <Route path='/edit-entry/:id' component={EditEntry}/>
             </Router>
-        </div>
+        </Box>
     }
 }
 
