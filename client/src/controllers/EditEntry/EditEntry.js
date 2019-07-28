c76d29f5fd5d0f9b0e1ff2f0e7d178b85203ad67
dzelga <dorota.zelga@gmail.com>
2019-07-28 08:38:12 +0200

    EditEntry > form styled with material-ui

--- client/src/controllers/EditEntry/EditEntry.js
+++ client/src/controllers/EditEntry/EditEntry.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import axios from 'axios';
 import api from "../../api.json"
 import findInvalidValues from "../../invalidValues"
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 class EditEntry extends Component {
     constructor() {
@@ -47,18 +49,47 @@ class EditEntry extends Component {
     };
 
     render() {
-        return <div>
-            <form onSubmit={this.handleSubmit}>
-                <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleChange}/>
-                <br/>
-                <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleChange}/>
-                <br/>
-                <input type="text" name="phoneNumber" value={this.state.phoneNumber} onChange={this.handleChange}/>
-                <br/>
-                <br/>
-                <input type="submit" value="Edit entry"/>
-            </form>
-        </div>
+        return <form noValidate autoComplete="off">
+            <TextField
+                required
+                id="standard-required"
+                label="First Name"
+                margin="normal"
+                InputLabelProps={{
+                    shrink: true,
+                }}
+                name="firstName"
+                value={this.state.firstName}
+                onChange={this.handleChange}
+            />
+            <TextField
+                required
+                id="standard-required"
+                label="Last Name"
+                margin="normal"
+                InputLabelProps={{
+                    shrink: true,
+                }}
+                name="lastName"
+                value={this.state.lastName}
+                onChange={this.handleChange}
+            />
+            <TextField
+                required
+                id="standard-required"
+                label="Phone number"
+                margin="normal"
+                InputLabelProps={{
+                    shrink: true,
+                }}
+                name="phoneNumber"
+                value={this.state.phoneNumber}
+                onChange={this.handleChange}
+                helperText="Example: +32 36 258741"
+            />
+            <br/>
+            <Button variant="contained" onClick={this.handleSubmit} color="primary">Edit Entry</Button>
+        </form>
     }
 }
 
