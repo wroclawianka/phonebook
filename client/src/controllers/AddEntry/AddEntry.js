c624e39c4040fb76a11f495bf4550d3985d71aa9
dzelga <dorota.zelga@gmail.com>
2019-07-28 09:32:21 +0200

    AddEntry > caption replaced alert()

--- client/src/controllers/AddEntry/AddEntry.js
+++ client/src/controllers/AddEntry/AddEntry.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import axios from 'axios';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import {Typography} from "@material-ui/core";
+import './AddEntry.css'
 import api from "../../api.json"
 import findInvalidValues from "../../invalidValues"
 
@@ -11,7 +13,8 @@ class AddEntry extends Component {
         this.state = {
             firstName: "",
             lastName: "",
-            phoneNumber: ""
+            phoneNumber: "",
+            alert: ""
         }
     }
 
@@ -25,7 +28,9 @@ class AddEntry extends Component {
         event.preventDefault();
         let invalidValues = findInvalidValues(this.state);
         if (invalidValues.length) {
-            alert(`invalid values: ${invalidValues}`)
+            this.setState({
+                alert: `invalid values: ${invalidValues}`
+            })
         } else {
             const data = {
                 firstName: this.state.firstName,
@@ -38,7 +43,11 @@ class AddEntry extends Component {
 
     saveEntry = (data) => {
         axios.post(api.url + '/post/entry', data)
-            .then(() => alert("Entry added"))
+            .then(() => {
+                this.setState({
+                    alert: `Entry added`
+                })
+            })
             .catch((err) => console.error(err));
     };
 
@@ -70,6 +79,8 @@ class AddEntry extends Component {
             />
             <br/>
             <Button variant="contained" onClick={this.handleSubmit} color="primary">Add Entry</Button>
+            <br/>
+            <Typography className="alert" variant="caption">{this.state.alert}</Typography>
         </form>
     }
 }
