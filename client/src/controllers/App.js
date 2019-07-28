715d45f61cdfedbcba8d63755c65df587ec06508
dzelga <dorota.zelga@gmail.com>
2019-07-28 08:51:38 +0200

    App.js > Container replaced Box

--- client/src/controllers/App.js
+++ client/src/controllers/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import 'typeface-roboto';
-import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import HomePage from './HomePage/HomePage.js'
 import AddEntry from './AddEntry/AddEntry'
@@ -10,7 +10,7 @@ import EditEntry from './EditEntry/EditEntry'
 
 class App extends Component {
     render() {
-        return <Box component="div">
+        return <Container maxWidth="md">
             <Typography variant="h3">
                 <a className="homepage-link" href="/">Phonebook</a>
             </Typography>
@@ -19,7 +19,7 @@ class App extends Component {
                 <Route path='/add-entry' component={AddEntry}/>
                 <Route path='/edit-entry/:id' component={EditEntry}/>
             </Router>
-        </Box>
+        </Container>
     }
 }
 
