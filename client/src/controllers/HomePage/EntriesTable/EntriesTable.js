65a717be0ec8fa0fe76683851a32f8aba3001a09
dzelga <dorota.zelga@gmail.com>
2019-07-28 08:48:47 +0200

    EntriesTable > table styled with material-ui

--- client/src/controllers/HomePage/EntriesTable/EntriesTable.js
+++ client/src/controllers/HomePage/EntriesTable/EntriesTable.js
@@ -1,35 +1,41 @@
 import React, {Component} from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 import './EntriesTable.css';
 
 class EntriesTable extends Component {
 
     render() {
-        return <div>
-            <table>
-                <thead>
-                <tr>
-                    <td>First Name</td>
-                    <td>Last Name</td>
-                    <td>Phone Number</td>
-                    <td></td>
-                </tr>
-                </thead>
-                <tbody>
-                {this.props.entries.map((entry) => {
-                    return (
-                        <tr key={entry._id}>
-                            <td>{entry.firstName}</td>
-                            <td>{entry.lastName}</td>
-                            <td>{entry.phoneNumber}</td>
-                            <td><a href={`/edit-entry/${entry._id}`}>[edit]</a></td>
-                        </tr>
-                    )
-                })
-                }
-                </tbody>
-            </table>
-        </div>
-
+        return <Paper>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>First Name</TableCell>
+                        <TableCell>Last Name</TableCell>
+                        <TableCell>Phone Number</TableCell>
+                        <TableCell/>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {this.props.entries.map((entry) => {
+                        return (
+                            <TableRow key={entry._id}>
+                                <TableCell component="th" scope="row">{entry.firstName}</TableCell>
+                                <TableCell component="th" scope="row">{entry.lastName}</TableCell>
+                                <TableCell component="th" scope="row">{entry.phoneNumber}</TableCell>
+                                <TableCell component="th" scope="row"><a
+                                    href={`/edit-entry/${entry._id}`}>edit</a></TableCell>
+                            </TableRow>
+                        )
+                    })
+                    }
+                </TableBody>
+            </Table>
+        </Paper>
     }
 }
 
