416922071dcf34f77c10e59dd06df5499793612a
dzelga <dorota.zelga@gmail.com>
2019-07-28 08:49:19 +0200

    EntriesTable.css file removed > not in use anymore

--- client/src/controllers/HomePage/EntriesTable/EntriesTable.js
+++ client/src/controllers/HomePage/EntriesTable/EntriesTable.js
@@ -5,7 +5,6 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import './EntriesTable.css';
 
 class EntriesTable extends Component {
 
