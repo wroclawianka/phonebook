f7c08b5ad36d3ae6d6dc03afcf7599c8ac6575d8
dzelga <dorota.zelga@gmail.com>
2019-07-28 08:09:03 +0200

    App.js > header > Typography used

--- client/src/controllers/App.js
+++ client/src/controllers/App.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
+import 'typeface-roboto';
 import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 import HomePage from './HomePage/HomePage.js'
 import AddEntry from './AddEntry/AddEntry'
 import EditEntry from './EditEntry/EditEntry'
@@ -9,7 +11,9 @@ import EditEntry from './EditEntry/EditEntry'
 class App extends Component {
     render() {
         return <Box component="div">
-            <h1><a className="homepage-link" href="/">Phonebook</a></h1>
+            <Typography variant="h3">
+                <a className="homepage-link" href="/">Phonebook</a>
+            </Typography>
             <Router>
                 <Route exact path='/' component={HomePage}/>
                 <Route path='/add-entry' component={AddEntry}/>
